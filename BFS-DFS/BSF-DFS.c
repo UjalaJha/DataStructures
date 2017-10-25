4240b395936cdf652ffdb21ffaaf2fd1839256e4
Sagar Ganiga <2015sagar.ganiga@ves.ac.in>
2017-10-25 23:25:13 +0530

    BFS-DFS

--- BFS-DFS/BSF-DFS.c
+++ BFS-DFS/BSF-DFS.c
@@ -1,96 +1,173 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node
-{
-    struct node *next;
-    int vertex;
-} node;
+#define NUM_VERTEX 10
 
-node *G[20];
-//heads of linked list
-int visited[20];
-int n;
-void read_graph();
-//create adjacency list
-void insert(int,int);
-//insert an edge (vi,vj) in te adjacency list
-void DFS(int);
+struct Vertex {
+	char name;
+	int mark;
+	struct Node* list;
+};
+struct Node {
+	struct Vertex* vertex;
+	struct Node* next;
+};
+struct Queue {
+};
+typedef struct Vertex Vertex;
+typedef struct Node Node;
 
-void main()
-{
-    int i;
-    read_graph();
-    //initialised visited to 0
-
-    for(i=0; i<n; i++)
-        visited[i]=0;
-
-    DFS(0);
+// creates a new vertex and return pointer to it
+Vertex* new_vertex(char name) {
+	Vertex* vertex = (Vertex*)malloc(sizeof(Vertex));
+	vertex->name = name;
+	vertex->list = 0;
+	return vertex;
 }
 
-void DFS(int i)
-{
-    node *p;
-
-    printf("\n%d",i);
-    p=G[i];
-    visited[i]=1;
-    while(p!=NULL)
-    {
-        i=p->vertex;
-
-        if(!visited[i])
-            DFS(i);
-        p=p->next;
-    }
+// connect the vertex a to vertex b
+// by adding b to a's linked list!
+void do_connect(Vertex* a, Vertex* b) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->vertex = b;
+	node->next = a->list;
+	a->list = node;
 }
 
-void read_graph()
-{
-    int i,vi,vj,no_of_edges;
-    printf("Enter number of vertices:");
-
-    scanf("%d",&n);
-
-    //initialise G[] with a null
-
-    for(i=0; i<n; i++)
-    {
-        G[i]=NULL;
-        //read edges and insert them in G[]
-
-        printf("Enter number of edges:");
-        scanf("%d",&no_of_edges);
-
-        for(i=0; i<no_of_edges; i++)
-        {
-            printf("Enter an edge(u,v):");
-            scanf("%d%d",&vi,&vj);
-            insert(vi,vj);
-        }
-    }
+// connects a to b and b to a
+void connect(Vertex* a, Vertex* b) {
+	do_connect (a, b);
+	do_connect (b, a);
 }
 
-void insert(int vi,int vj)
-{
-    node *p,*q;
+// visit function
+void visit(Vertex* vertex) {
+	printf ("\n%5d. %c", vertex->mark, vertex->name);
+}
 
-    //acquire memory for the new node
-    q=(node*)malloc(sizeof(node));
-    q->vertex=vj;
-    q->next=NULL;
+// do a depth first search
+void do_dfs(Vertex *vertex, int* count) {
+	Node* p = vertex->list;
+	vertex->mark = ++(*count);
+	visit (vertex);
+	while (p != 0) {
+		if (!p->vertex->mark) {
+			do_dfs (p->vertex, count);
+		}
+		p = p->next;
+	}
+}
+void dfs(Vertex *graph[]) {
+	int i;
+	int count = 0;
+	// set all to unvisited
+	for (i = 0; i < NUM_VERTEX; i ++) {
+		graph[i]->mark = 0;
+	}
+	// each vertex dfs it
+	for (i = 0; i < NUM_VERTEX; i ++) {
+		if (graph[i]->mark == 0) {
+			do_dfs (graph[i], &count);
+		}
+	}
+}
 
-    //insert the node in the linked list number vi
-    if(G[vi]==NULL)
-        G[vi]=q;
-    else
-    {
-        //go to end of the linked list
-        p=G[vi];
+// breadth first search
+void do_bfs(Vertex *vertex, int* count) {
+	Node *initialQueue, *front, *rear, *p, *temp;
+	Vertex *w;
+	// count = count + 1, mark vertex with count
+	vertex->mark = ++(*count);
+	visit (vertex);
+	// initialize a queue, and initialize with vertex.
+	initialQueue = (Node*)malloc(sizeof(Node));
+	initialQueue->vertex = vertex;
+	initialQueue->next = 0;
+	front = initialQueue;
+	rear  = initialQueue;
+	// while queue is not empty
+	while (front != 0) {
+		// for vertyex w in V adjacent to the front vertex
+		p = front->vertex->list;
+		while (p != 0) {
+			w = p->vertex;
+			// if w is marked with 0
+			if (w->mark == 0) {
+				Node* nextQueue;
+				// count = count + 1, mark w with count
+				w->mark = ++(*count);
+				visit (w);
+				// add w to queue
+				nextQueue = (Node*)malloc(sizeof(Node));
+				nextQueue->vertex = w;
+				nextQueue->next = 0;
+				rear->next = nextQueue;
+				rear = nextQueue;
+			}
+			p = p->next;
+		}
+		// remove front vertex from queue
+		temp = front;
+		front = front->next;
+		free (temp);
+	}
+}
+void bfs(Vertex *graph[]) {
+	int i;
+	int count = 0;
+	// set all to unvisited
+	for (i = 0; i < NUM_VERTEX; i ++) {
+		graph[i]->mark = 0;
+	}
+	// each vertex bfs it
+	for (i = 0; i < NUM_VERTEX; i ++) {
+		if (graph[i]->mark == 0) {
+			do_bfs (graph[i], &count);
+		}
+	}
+}
 
-        while(p->next!=NULL)
-            p=p->next;
-        p->next=q;
-    }
+// main function
+int main() {
+
+	// create a adjacency list
+	Vertex *graph[NUM_VERTEX] = {
+		new_vertex('A'), //[0]
+		new_vertex('B'), //[1]
+		new_vertex('C'), //[2]
+		new_vertex('D'), //[3]
+		new_vertex('E'), //[4]
+		new_vertex('F'), //[5]
+		new_vertex('G'), //[6]
+		new_vertex('H'), //[7]
+		new_vertex('I'), //[8]
+		new_vertex('J')  //[9]
+	};
+
+	// connect the vertices
+	connect(graph[0], graph[2]);
+	connect(graph[0], graph[3]);
+	connect(graph[0], graph[4]);
+	connect(graph[1], graph[4]);
+	connect(graph[1], graph[5]);
+	connect(graph[2], graph[3]);
+	connect(graph[2], graph[5]);
+	connect(graph[4], graph[5]);
+	connect(graph[6], graph[7]);
+	connect(graph[6], graph[9]);
+	connect(graph[7], graph[8]);
+	connect(graph[8], graph[9]);
+
+	// depth first search
+	printf ("DFS: ");
+	dfs (graph);
+
+	// breadth first search
+	printf ("\n\n");
+	printf ("BFS: ");
+	bfs (graph);
+
+	printf ("\n\nAnd that's it!!");
+	getchar ();
+
 }
